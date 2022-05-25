c50de1029e2b2661d9e33a450d717c0ba5c5b28b
Nataliia Huebler <nataliia.huebler@yahoo.com>
2022-05-25 12:43:19 +0200

    update

--- code/compare_stability.R
+++ code/compare_stability.R
@@ -38,45 +38,199 @@ morphology_joined_long <- join_morphosyntax_with_stability(morphology_K4_feature
 
 syntax_joined_long <- join_morphosyntax_with_stability(syntax_K4_feature_contributions)
 
+
+
 select_plot_Japonic <- function(dataset_joined_long){
   dataset_joined_long %>%
-    filter(ancestry == "Japonic", family == "p1_japonic") %>%
-    ggplot(aes(reconstructed_present, proportion_ancestry)) +
-    geom_point(color = "#E78AC3", size = 2.5) +
-    labs(x = element_blank(), y = element_blank())
+    filter(ancestry == "Japonic" | ancestry == "Japono_Koreanic" , family == "p1_japonic") %>%
+    ggplot(aes(reconstructed_present, proportion_ancestry, fill = family)) +
+    geom_point(color = "#E78AC3", size = 2.5, alpha = 0.5) +
+    labs(x = 'Reconstruction', y = 'Ancestry')
 }
 
 select_plot_Turkic <- function(dataset_joined_long){
   dataset_joined_long %>%
     filter(ancestry == "Turkic", family == "p1_turkic") %>%
-    ggplot(aes(reconstructed_present, proportion_ancestry)) +
+    ggplot(aes(reconstructed_present, proportion_ancestry, fill = family)) +
     geom_point(color = "#FC8D62", size = 2.5, alpha = 0.5) +
-    labs(x = element_blank(), y = element_blank())
+    labs(x = 'Reconstruction', y = 'Ancestry')
 }
 
 select_plot_Tungusic <- function(dataset_joined_long){
   dataset_joined_long %>%
     filter(ancestry == "Tungusic", family == "p1_tungusic") %>%
-    ggplot(aes(reconstructed_present, proportion_ancestry)) +
+    ggplot(aes(reconstructed_present, proportion_ancestry, fill = family)) +
     geom_point(color = "#66C2A5", size = 2.5, alpha = 0.5) +
-    labs(x = element_blank(), y = element_blank())
+    labs(x = 'Reconstruction', y = 'Ancestry')
 }
 
 select_plot_Mongolic <- function(dataset_joined_long){
   dataset_joined_long %>%
-    filter(ancestry == "Mongolic", family == "p1_mongolic") %>%
-    ggplot(aes(reconstructed_present, proportion_ancestry)) +
+    filter(ancestry == "Mongolic"| ancestry == "Mongolo_Koreanic", family == "p1_mongolic") %>%
+    ggplot(aes(reconstructed_present, proportion_ancestry, fill = family)) +
     geom_point(color = "#8DA0CB", size = 2.5, alpha = 0.5) +
-    labs(x = element_blank(), y = element_blank())
+    labs(x = 'Reconstruction', y = 'Ancestry')
 }
+
+select_plot_Koreanic <- function(dataset_joined_long){
+  dataset_joined_long %>%
+    filter(ancestry == "Mongolo_Koreanic" | ancestry == "Japono_Koreanic" , family == "p1_koreanic") %>%
+    ggplot(aes(reconstructed_present, proportion_ancestry, fill = family)) +
+    geom_point(color = "#41B6C4", size = 2.5, alpha = 0.5)  +
+    labs(x = 'Reconstruction', y = 'Ancestry')
+}
+
+
+# Plot phonology
+
+phonology_turkic_plot <- phonology_joined_long %>%
+  filter(ancestry == "Turkic", family == "p1_turkic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry)) +
+  geom_point(color = "#FC8D62", size = 2.5, alpha = 0.5) +
+  labs(x = element_blank(), y = 'Ancestry') +
+  theme(legend.position="right") +
+  ggtitle("Phonology")
+
+phonology_mongolic_plot <- phonology_joined_long %>%
+  filter(ancestry == "Mongolic"| ancestry == "Mongolo_Koreanic", family == "p1_mongolic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry)) +
+  geom_point(color = "#8DA0CB", size = 2.5, alpha = 0.5) +
+  labs(x = element_blank(), y = 'Ancestry') +
+  ylim(0,1)
+
+phonology_tungusic_plot <- phonology_joined_long %>%
+  filter(ancestry == "Tungusic", family == "p1_tungusic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry)) +
+  geom_point(color = "#66C2A5", size = 2.5, alpha = 0.5) +
+  labs(x = element_blank(), y = 'Ancestry')
+
+phonology_koreanic_plot <- phonology_joined_long %>%
+  filter(ancestry == "Mongolo_Koreanic" | ancestry == "Japono_Koreanic" , family == "p1_koreanic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry)) +
+  geom_point(color = "#41B6C4", size = 2.5, alpha = 0.5)  +
+  labs(x = element_blank(), y = 'Ancestry') +
+  ylim(0,1)
+
+phonology_japonic_plot <- phonology_joined_long %>%
+  filter(ancestry == "Japonic" | ancestry == "Japono_Koreanic" , family == "p1_japonic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry)) +
+  geom_point(color = "#E78AC3", size = 2.5, alpha = 0.5) +
+  labs(x = 'Reconstruction', y = 'Ancestry') +
+  xlim(0,1)
+
+# Plot morphology
+
+morphology_turkic_plot <- morphology_joined_long %>%
+  filter(ancestry == "Turkic", family == "p1_turkic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry)) +
+  geom_point(color = "#FC8D62", size = 2.5, alpha = 0.5) +
+  labs(x = element_blank(), y = element_blank()) +
+  ggtitle("Morphology")
+
+morphology_mongolic_plot <- morphology_joined_long %>%
+  filter(ancestry == "Mongolic"| ancestry == "Mongolo_Koreanic", family == "p1_mongolic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry)) +
+  geom_point(color = "#8DA0CB", size = 2.5, alpha = 0.5) +
+  labs(x = element_blank(), y = element_blank()) + 
+  labs(x = element_blank(), y = element_blank())
+
+morphology_tungusic_plot <- morphology_joined_long %>%
+  filter(ancestry == "Tungusic", family == "p1_tungusic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry)) +
+  geom_point(color = "#66C2A5", size = 2.5, alpha = 0.5) +
+  labs(x = 'Reconstruction', y = 'Ancestry') + 
+  labs(x = element_blank(), y = element_blank())
+
+morphology_koreanic_plot <- morphology_joined_long %>%
+  filter(ancestry == "Mongolo_Koreanic" | ancestry == "Japono_Koreanic" , family == "p1_koreanic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry)) +
+  geom_point(color = "#41B6C4", size = 2.5, alpha = 0.5)  +
+  labs(x = element_blank(), y = element_blank())
+
+morphology_japonic_plot <- morphology_joined_long %>%
+  filter(ancestry == "Japonic" | ancestry == "Japono_Koreanic" , family == "p1_japonic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry)) +
+  geom_point(color = "#E78AC3", size = 2.5, alpha = 0.5) +
+  labs(x = "Reconstruction", y = element_blank())
+
+# Plot syntax
+syntax_turkic_plot <- syntax_joined_long %>%
+  filter(ancestry == "Turkic", family == "p1_turkic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry, fill = family)) +
+  geom_point(color = "#FC8D62", size = 2.5, alpha = 0.5) +
+  ggtitle("Syntax") + 
+  labs(x = element_blank(), y = element_blank()) + 
+  scale_fill_manual(name = element_blank(),
+                    labels = c("Turkic"),
+                    values = c("#FC8D62")) +
+  theme(legend.position="right", legend.text=element_text(size=12))
+
+syntax_tungusic_plot <- syntax_joined_long %>%
+  filter(ancestry == "Tungusic", family == "p1_tungusic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry, fill = family)) +
+  geom_point(color = "#66C2A5", size = 2.5, alpha = 0.5) +
+  labs(x = element_blank(), y = element_blank()) + 
+  scale_fill_manual(name = element_blank(),
+                    labels = c("Tungusic"),
+                    values = c("#66C2A5")) +
+  theme(legend.position="right", legend.text=element_text(size=12))
+
+syntax_mongolic_plot <- syntax_joined_long %>%
+  filter(ancestry == "Mongolic"| ancestry == "Mongolo_Koreanic", family == "p1_mongolic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry, fill = family)) +
+  geom_point(color = "#8DA0CB", size = 2.5, alpha = 0.5) +
+  labs(x = element_blank(), y = element_blank()) +
+  scale_fill_manual(name = element_blank(),
+                    labels = c("Mongolic"),
+                    values = c("#8DA0CB")) +
+  theme(legend.position="right", legend.text=element_text(size=12))
+
+syntax_koreanic_plot <- syntax_joined_long %>%
+  filter(ancestry == "Mongolo_Koreanic" | ancestry == "Japono_Koreanic" , family == "p1_koreanic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry, fill = family)) +
+  geom_point(color = "#41B6C4", size = 2.5, alpha = 0.5)  +
+  labs(x = element_blank(), y = element_blank()) +
+  scale_fill_manual(name = element_blank(),
+                    labels = c("Koreanic"),
+                    values = c("#41B6C4")) +
+  theme(legend.position="right", legend.text=element_text(size=12))
+
+syntax_japonic_plot <- syntax_joined_long %>%
+  filter(ancestry == "Japonic" | ancestry == "Japono_Koreanic" , family == "p1_japonic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry, fill = family)) +
+  geom_point(color = "#E78AC3", size = 2.5, alpha = 0.5) +
+  labs(x = "Reconstruction", y = element_blank()) + 
+  scale_fill_manual(name = element_blank(),  
+                    labels = c("Japonic"), 
+                    values = c("#E78AC3")) +
+  theme(legend.position="right", legend.text=element_text(size=12))
+
+# Save the plot
+ggsave("plots/reconstruction.pdf", (phonology_turkic_plot | morphology_turkic_plot | syntax_turkic_plot) / 
+        ( phonology_mongolic_plot | morphology_mongolic_plot | syntax_mongolic_plot) /
+        ( phonology_tungusic_plot | morphology_tungusic_plot | syntax_tungusic_plot ) /
+        ( phonology_koreanic_plot | morphology_koreanic_plot | syntax_koreanic_plot) /
+        ( phonology_japonic_plot | morphology_japonic_plot | syntax_japonic_plot),
+          height=9,width=8)
+
+
+phonology_joined_long %>%
+  filter(ancestry == "Turkic", family == "p1_turkic") %>%
+  ggplot(aes(reconstructed_present, proportion_ancestry, fill = family)) +
+  geom_point(color = "#FC8D62", size = 2.5, alpha = 0.5) +
+  labs(x = 'Reconstruction', y = 'Ancestry') +
+  scale_fill_manual(name = element_blank(), 
+                     labels = c("Turkic"),
+                     values = c("#FC8D62")) +
+  theme(legend.position="right")
+
 
 select_plot_Mongolo_Koreanic <- function(dataset_joined_long){
   dataset_joined_long %>%
     filter(ancestry == "Mongolo_Koreanic", family == "p1_mongolic" | family == "p1_koreanic") %>%
     ggplot(aes(reconstructed_present, proportion_ancestry, color = family)) +
     geom_point(size = 2.5, alpha = 0.5) +
-    #theme(axis.text.x = element_blank(), axis.text.y = element_blank()) +
-    labs(x = element_blank(), y = element_blank()) +
+    labs(x = 'Reconstruction', y = 'Ancestry') + 
     scale_color_manual(name = element_blank(), 
                        labels = c("K", "M"), 
                        values = c("#41B6C4","#8DA0CB")) +
@@ -89,30 +243,11 @@ select_plot_Japono_Koreanic <- function(dataset_joined_long){
     filter(ancestry == "Japono_Koreanic", family == "p1_japonic" | family == "p1_koreanic") %>%
     ggplot(aes(reconstructed_present, proportion_ancestry, color = family)) +
     geom_point(size = 2.5, alpha = 0.5) +
-    labs(x = element_blank(), y = element_blank()) + 
+    labs(x = 'Reconstruction', y = 'Ancestry') + 
     scale_color_manual(name = element_blank(), 
                        labels = c("J", "K"),  
                        values = c("#E78AC3","#41B6C4")) +
     theme(legend.position="bottom")
 }
 
-phonology_japonic_plot <- select_plot_Japonic(phonology_joined_long) + ylab("Japonic/Koreanic")
-phonology_turkic_plot <- select_plot_Turkic(phonology_joined_long) + ggtitle("Phonology") + ylab("Turkic")
-phonology_tungusic_plot <- select_plot_Tungusic(phonology_joined_long) + ylab("Tungusic")
 phonology_mongolo_koreanic_plot <- select_plot_Mongolo_Koreanic(phonology_joined_long) + ylab("Mongolic/Koreanic")
-
-morphology_turkic_plot <- select_plot_Turkic(morphology_joined_long) + ggtitle("Morphology")
-morphology_tungusic_plot <- select_plot_Tungusic(morphology_joined_long)
-morphology_japono_koreanic_plot <- select_plot_Japono_Koreanic(morphology_joined_long)
-morphology_mongolic_plot <- select_plot_Mongolic(morphology_joined_long) 
-
-syntax_turkic_plot <- select_plot_Turkic(syntax_joined_long) + ggtitle("Syntax")
-syntax_tungusic_plot <- select_plot_Tungusic(syntax_joined_long)
-syntax_mongolic_plot <- select_plot_Mongolic(syntax_joined_long) 
-syntax_japono_koreanic_plot <- select_plot_Japono_Koreanic(syntax_joined_long)
-
-ggsave("plots/reconstruction.pdf", (phonology_turkic_plot | morphology_turkic_plot | syntax_turkic_plot) / 
-        ( phonology_tungusic_plot | morphology_tungusic_plot | syntax_tungusic_plot ) /
-        ( phonology_japonic_plot | morphology_japono_koreanic_plot | syntax_japono_koreanic_plot) /
-        ( phonology_mongolo_koreanic_plot | morphology_mongolic_plot | syntax_mongolic_plot), 
-          height=8,width=6)
